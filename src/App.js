ea658dd217fd19efeb14dc69d35a3fc41ded6e32
SanathHerath6014 <sanath6014nk@gmail.com>
2022-10-09 09:37:50 +0530

    pala yanna

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import BottomBar from './components/BottomBar';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <NavBar/>
+      <Home/>
+      <About/>
+      <Contact/>
+      <BottomBar/>
+    </div>
   );
 }
 
